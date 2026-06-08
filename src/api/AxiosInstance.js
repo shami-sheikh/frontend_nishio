ce59209d5db928f947b08bddf8523826254bf10e
shami-sheikh <your-github-samidev.co@gmail.com>
2026-06-08 12:16:42 +0530

    adding render into backend

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance=axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://backend-nishio.onrender.com/api"
 })
 // automatically token add karo every request mein
 axiosInstance.interceptors.request.use((config)=>{
